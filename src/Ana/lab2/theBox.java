f1429d68e5ffd596c3908950ae17ef6877777edd
Ana Coroletchi <ana@MacBook-Air-Ana.local>
2021-10-04 15:24:51 +0300

    class added

---
+++ src/Ana/lab2/theBox.java
@@ -0,0 +1,42 @@
+package Ana.lab2;
+
+//theBox class
+
+public class theBox {
+    double height;
+    double width;
+    double depth;
+
+    // Without parameters
+    public theBox() {
+        height = 1;
+        width = 1;
+        depth = 1;
+    }
+
+    // With 1 parameter
+    public theBox(double value) {
+        height = value;
+        width = value;
+        depth = value;
+    }
+
+    // With 3 parameters
+    public theBox(double height, double width, double depth) {
+        this.height = height;
+        this.width = width;
+        this.depth = depth;
+    }
+
+    // Calculating the surface
+    public double Area() {
+        return 2 * (height * width + height * depth + width * depth);
+    }
+
+    // Calculating the volume
+    public double Volume() {
+        return height * width * depth;
+    }
+
+}
+
