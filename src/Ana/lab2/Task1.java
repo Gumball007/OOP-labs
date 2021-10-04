b036a360b57049d649bf03478639d9dcca698fcf
Ana Coroletchi <ana@MacBook-Air-Ana.local>
2021-10-04 15:24:11 +0300

    Task 1 added

---
+++ src/Ana/lab2/Task1.java
@@ -0,0 +1,33 @@
+package Ana.lab2;
+
+public class Task1 {
+    public static void main(String[]args){
+        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+
+        theBox Box1 = new theBox();
+        System.out.println("---Box 1---");
+        System.out.println("The area of the Box1 is: " + Box1.Area() + " cm^2" );
+        System.out.println("The volume of the Box1 is: " + Box1.Volume() + " cm^3" );
+
+        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+
+        theBox Box2 = new theBox(7.1);
+        System.out.println("---Box 2---");
+        System.out.println("The area of the Box2 is: " + Box2.Area() + " cm^2" );
+        System.out.println("The volume of the Box2 is: " + Box2.Volume() + " cm^3" );
+
+        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+
+        theBox Box3 = new theBox(5.2, 2, 4);
+        System.out.println("---Box 3---");
+        System.out.println("The area of the Box3 is: " + Box3.Area() + " cm^2" );
+        System.out.println("The volume of the Box3 is: " + Box1.Volume() + " cm^3" );
+
+        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+
+
+
+    }
+}
+
+
